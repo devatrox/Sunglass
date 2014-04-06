c3ddcbbafa422a9eb84245f8938a1cc3a8a8c472
Jason Nickel <mail@jasonnickel.de>
2014-04-07 00:15:12 +0200

    So many changes... I'm scared

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    bwr: grunt.file.readJSON("bower.json"),
+
+    concat: {
+      dist: {
+        options: {
+          banner: "@if $add-copyright {\n" +
+                  "/*!\n" +
+                  "* <%= bwr.name %> - <%= bwr.description %>\n" +
+                  "* v<%= bwr.version %>\n" +
+                  "* <%= bwr.homepage %>\n" +
+                  "*/\n" +
+                  "}\n\n"
+        },
+        src: ["src/core/_defaults.scss", "src/**/*.scss"],
+        dest: "_sunglass.scss"
+      }
+    },
+
+    sass: {
+      test: {
+        options: {
+          noCache: true,
+          loadPath: ".",
+          bundleExec: true
+        },
+        files: [
+          {
+            expand: true,
+            src: ["tests/**/*.scss"],
+            dest: ".",
+            ext: ".css"
+          }
+        ]
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+        spawn: false
+      },
+      sass: {
+        files: ["tests/**/*.scss", "src/**/*.scss"],
+        tasks: ["sass"]
+      },
+      html: {
+        files: ["tests/**/index.html"]
+      }
+    },
+
+    connect: {
+      test: {
+        options: {
+          port: 9000,
+          base: "test"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-connect");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask("compile", ["concat", "sass"]);
+  grunt.registerTask("default", ["watch"]);
+};
