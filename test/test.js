348453e7ed1cb6eb646353ce4a2f1afbae1cc3ce
Jason <jn@alpha9marketing.com>
2015-12-19 20:35:24 +0100

    Adding mathsass as dev-dependency

--- test/test.js
+++ test/test.js
@@ -4,5 +4,6 @@ var sassTrue = require('sass-true');
 var sassFile = path.join(__dirname, 'test.scss');
 
 sassTrue.runSass({
-	file: sassFile
+	file: sassFile,
+	includePaths: ['node_modules/mathsass/dist'],
 }, describe, it);
